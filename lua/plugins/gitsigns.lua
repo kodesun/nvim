6be83dc10f241f0ed7897e22632b86c2cd169c46
kodesun <168811279+kodesun@users.noreply.github.com>
2024-10-22 20:40:28 -0700

    feature: gitsigns hunk keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -12,5 +12,10 @@ return {
 
     config = function ()
         require('gitsigns').setup()
+
+        -- Keymappings
+        vim.keymap.set('n', '<leader>gha', "<cmd>:Gitsigns stage_hunk<CR>", {desc = '[G]it [H]unk [S]tage'})
+        vim.keymap.set('n', '<leader>ghu', "<cmd>:Gitsigns undo_stage_hunk<CR>", {desc = '[G]it [H]unk [U]nstage'})
+        vim.keymap.set('n', '<leader>ghp', "<cmd>:Gitsigns preview_hunk<CR>", {desc = '[G]it [H]unk [P]review'})
     end
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,14 +2,23 @@ return {
   "folke/which-key.nvim",
   preset = "helix",
   config = function()
-      require('which-key').register {
-          ['<leader>c'] = { name = '[C]omment', _ = 'which_key_ignore' },
-          ['<leader>d'] = { name = '[D]iagnostics', _ = 'which_key_ignore' },
-          ['<leader>f'] = { name = '[F]ile Functions', _ = 'which_key_ignore' },
-          ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-          ['<leader>l'] = { name = '[L]SP', _ = 'which_key_ignore' },
-          ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      }
+      local wk = require('which-key')
+      wk.add ({
+          { "<leader>c", group = "[C]omment" },
+          { "<leader>c_", hidden = true },
+          { "<leader>d", group = "[D]iagnostics" },
+          { "<leader>d_", hidden = true },
+          { "<leader>f", group = "[F]ile Functions" },
+          { "<leader>f_", hidden = true },
+          { "<leader>g", group = "[G]it" },
+          { "<leader>g_", hidden = true },
+          { "<leader>gh", group = "[G]it [H]unk" },
+          { "<leader>gh_", hidden = true },
+          { "<leader>l", group = "[L]SP" },
+          { "<leader>l_", hidden = true },
+          { "<leader>t", group = "[T]oggle" },
+          { "<leader>t_", hidden = true },
+      })
 
       vim.keymap.set('n', '?', '<cmd>:WhichKey<CR>', { desc = 'WhichKey?' })
   end,
